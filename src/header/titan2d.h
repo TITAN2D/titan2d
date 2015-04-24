b5fdad20f09c6ee6d58425ba5c7ad78ffe5b6210
Nikolay Simakov <ns@compmodel.org>
2015-04-23 22:46:37 -0400

    add titan2d.h currently mainly for version number

---
+++ src/header/titan2d.h
@@ -0,0 +1,25 @@
+/*******************************************************************
+ * Copyright (C) 2003 University at Buffalo
+ *
+ * This software can be redistributed free of charge.  See COPYING
+ * file in the top distribution directory for more details.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Author: 
+ * Description: 
+ *
+ *******************************************************************
+ * $Id: constant.h 129 2007-06-07 19:54:28Z dkumar $ 
+ */
+
+#ifndef TITAN2D
+#define TITAN2D
+
+#define TITAN2D_VERSION_MAJOR 3
+#define TITAN2D_VERSION_MINOR 0
+#define TITAN2D_VERSION_REVISION 1
+
+#endif
