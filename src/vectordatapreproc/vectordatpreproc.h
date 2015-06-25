67810c46813156dca944f63a80d1d6e1b696571a
Nikolay Simakov <ns@compmodel.org>
2015-06-25 00:46:34 -0400

    added header for vectordatpreproc

---
+++ src/vectordatapreproc/vectordatpreproc.h
@@ -0,0 +1,25 @@
+/***************************************************************************
+ main.cpp  -  description
+ -------------------
+ begin                : Mon Feb 21 17:41:53 EST 2005
+ copyright            : (C) |YEAR| by Amrita Chanda
+ email                : achanda@buffalo.edu
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef VECTORDATPREPROC_H
+#define VECTORDATPREPROC_H
+
+
+int vectordatpreproc(const char * topomain, const char * toposub,
+        const char * topomapset, const char * topomap, const char * topovector);
+
+#endif
