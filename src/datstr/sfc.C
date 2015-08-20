b679cbb5f58a294fc60db43917acb1dc9e9cc655
Nikolay Simakov <ns@compmodel.org>
2015-08-20 11:12:47 -0400

    sfc_key_null is global variable now, at some point should be set to be constexpr

---
+++ src/datstr/sfc.C
@@ -0,0 +1,18 @@
+/*******************************************************************
+ * Copyright (C) 2003 University at Buffalo
+ *
+ * This software can be redistributed free of charge.  See COPYING
+ * file in the top distribution directory for more details.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Author:
+ * Description:
+ *
+ *******************************************************************/
+
+#include "../header/sfc.h"
+
+SFC_Key sfc_key_null=0;
