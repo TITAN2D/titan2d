3cb4ed90977095e4fe907b8f3cef31f03ac18259
Nikolay Simakov <ns@compmodel.org>
2015-09-10 11:10:16 -0400

    elenode.hpp header should be used instead of element,node and hashtable headers to avoid incompleate definition dependencies

---
+++ src/header/elenode.hpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   elenode.hpp
+ * Author: mikola
+ *
+ * Created on September 2, 2015, 12:56 PM
+ */
+
+#ifndef ELENODE_HPP
+#define	ELENODE_HPP
+
+//this header should be use instead of node.h element.h hashtab.h
+
+
+#include "node.h"
+#include "element2.h"
+#include "hashtab.h"
+
+#endif	/* ELENODE_HPP */
+
