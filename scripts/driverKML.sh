0194f9a72fa242fffa4b4982870f4c88917a891a
Nikolay Simakov <ns@compmodel.org>
2016-06-09 14:56:26 -0400

    set to use titan as python

--- scripts/driverKML.sh
+++ scripts/driverKML.sh
@@ -7,21 +7,23 @@
 fullpath="$(readlink -f $0)"
 TITAN2D_HOME="$(echo "$fullpath" | sed "s?/bin/driverKML.sh??")"
 TITAN2D_PYUTIL_DIR=$TITAN2D_HOME/lib/python2.7/site-packages
+TITAN2D_PYTHON=$TITAN2D_HOME/bin/titan
+
 
 cp $1/zone.txt $1/vizout/zone.txt
 cp $1/height_scale_for_KML.data $1/vizout/height_scale_for_KML.data
  
 echo "Running h5dump"
-python $TITAN2D_PYUTIL_DIR/h5dump.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/h5dump.py $1/vizout
 echo "Parsing XML"
-python $TITAN2D_PYUTIL_DIR/parseXML.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/parseXML.py $1/vizout
 echo "Combining Files"
-python $TITAN2D_PYUTIL_DIR/combineFiles.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/combineFiles.py $1/vizout
 # On VHub, FreeSerif.ttf font file is in the 
 # /usr/share/fonts/truetype/freefont directory.
 # Cannot assume this is true for all installations of the Titan2D GUI.
 # Look for FreeSerif.ttf font file in $TOOLDIR/bin directory.
 echo "Creating animated KML"
-python $TITAN2D_PYUTIL_DIR/createVolcanoKML_POLYGONS_ANIMATION.py $1/vizout $TITAN2D_PYUTIL_DIR
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/createVolcanoKML_POLYGONS_ANIMATION.py $1/vizout $TITAN2D_PYUTIL_DIR
 echo "Creating flat KML"
-python $TITAN2D_PYUTIL_DIR/createVolcanoKML_POLYGONS_LASTFRAME.py $1/vizout $TITAN2D_PYUTIL_DIR
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/createVolcanoKML_POLYGONS_LASTFRAME.py $1/vizout $TITAN2D_PYUTIL_DIR
--- scripts/driverMultiProcessor.sh
+++ scripts/driverMultiProcessor.sh
@@ -7,25 +7,28 @@
 fullpath="$(readlink -f $0)"
 TITAN2D_HOME="$(echo "$fullpath" | sed "s?/bin/driverMultiProcessor.sh??")"
 TITAN2D_PYUTIL_DIR=$TITAN2D_HOME/lib/python2.7/site-packages
+TITAN2D_PYTHON=$TITAN2D_HOME/bin/titan
+
+RUN_DIR=$1/vizout
 
 cp $1/Stdout.txt $1/vizout/Stdout.txt
 
 echo "Running h5dump"
-python $TITAN2D_PYUTIL_DIR/h5dump.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/h5dump.py $RUN_DIR
 echo "Parsing XML"
-python $TITAN2D_PYUTIL_DIR/parseXML.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/parseXML.py $RUN_DIR
 echo "Combining Files"
-python $TITAN2D_PYUTIL_DIR/combineFiles.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/combineFiles.py $RUN_DIR
 echo "Running boundariesMultiProcessor"
-python $TITAN2D_PYUTIL_DIR/boundariesMultiProcessor.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/boundariesMultiProcessor.py $RUN_DIR
 echo "Running lava"
-python $TITAN2D_PYUTIL_DIR/lava.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/lava.py $RUN_DIR
 echo "Running lava_points"
-python $TITAN2D_PYUTIL_DIR/lava_points.py $1/vizout
+$TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/lava_points.py $RUN_DIR
 
 echo "Running gnuplot.py"
 if [ $# == 2 ] ; then
-    python $TITAN2D_PYUTIL_DIR/gnuplot.py $1/vizout
+    $TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/gnuplot.py $RUN_DIR
 else
-    python $TITAN2D_PYUTIL_DIR/gnuplot.py $1/vizout $3
+    $TITAN2D_PYTHON $TITAN2D_PYUTIL_DIR/gnuplot.py $RUN_DIR $3
 fi
