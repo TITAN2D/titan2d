dcb814c2be9f4e8b63f02eaa14267c8697e7df4c
Nikolay Simakov <ns@compmodel.org>
2016-06-09 23:35:46 -0400

    added runing examples with all physical cores

--- share/titan2d_examples/run_all_examples.sh
+++ share/titan2d_examples/run_all_examples.sh
@@ -9,22 +9,26 @@ if [ -a "$wd/../../bin/titanvar" ]; then
     source "$wd/../../bin/titanvar"
 fi
 
+CORES_PER_SOCKET=`lscpu|grep "Core(s) per socket"|cut -d':' -f2-`
+SOCKETS=`lscpu|grep "Socket(s):"|cut -d':' -f2-`
+CORES_TO_USE=`expr $CORES_PER_SOCKET \* $SOCKETS`
+
 for d in Coulomb  Coulomb_MatMap
-do 
+do
 echo $d
 cd inclined/$d
 echo "In directory: " `pwd`
-echo "Running:" $titan -nt 4 input.py \>\& out_reg
-$titan -nt 4 input.py >& out_reg
+echo "Running:" $titan -nt $CORES_TO_USE input.py \>\& out_reg
+$titan -nt $CORES_TO_USE input.py >& out_reg
 cd $wd
 done
 
 for d in Coulomb Pouliquen_Forterre Voellmy_Salm Coulomb_with_stopping Coulomb_all_param_explicit Coulomb_FluxSrc_DischPlane Coulomb_GeoTIFF TwoPhases_Pitman_Le
-do 
+do
 echo $d
 cd colimafinemini/$d
 echo "In directory: " `pwd`
-echo "Running:" $titan -nt 4 input.py \>\& out_reg
-$titan -nt 4 input.py >& out_reg
+echo "Running:" $titan -nt $CORES_TO_USE input.py \>\& out_reg
+$titan -nt $CORES_TO_USE input.py >& out_reg
 cd $wd
 done
